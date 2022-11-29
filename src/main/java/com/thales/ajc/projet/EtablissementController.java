f572aceeb107e495be089f4c21bdfba0fc10fcf0
Renaud <vidalrenaud7@gmail.com>
2022-11-29 17:49:24 +0100

    transition vers pro

--- src/main/java/com/thales/ajc/projet/EtablissementController.java
+++ src/main/java/com/thales/ajc/projet/EtablissementController.java
@@ -16,6 +16,7 @@ import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -67,7 +68,11 @@ public class EtablissementController implements Initializable {
 
         //REDIRECTION VERS PROFFESSEUR
         idBoutonProfesseur.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> {
-
+            try {
+                SceneControler.switchScene(e, "Enseignant");
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
         });
 
         idButtonValiderEtablissement.getStyleClass().setAll("btn", "btn-primary");
